e9875e318f1c54fa0a06a46cb19af7d573a52e3a
AAraiza <alberto.araiza88@gmail.com>
2022-12-12 12:28:00 -0600

    trying webhook2

--- routes/clients.py
+++ routes/clients.py
@@ -8,7 +8,7 @@ clients = Blueprint("clients", __name__)
 @clients.route("/")
 def index():
     clients = Client.query.all()
-    return "Adios mundo"
+    return "Hola de nuevo mundo"
     #return render_template("index.html", clients = clients)
 
 @clients.route("/new",methods = ['POST'])
